d86c5dc3aae6a7f507c8f16b0c2b8a6eae27a26e
hkrVikrant <vikranttomartech@gmail.com>
2025-12-22 11:38:46 +0530

    update routes

--- src/routes/handle.routes.js
+++ src/routes/handle.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
+
+const authRoutes = require("./auth.routes");
 const userRoutes = require("./user.routes");
 
 const handleRoutes = express.Router();
 
+
+handleRoutes.use("/api", authRoutes);
 handleRoutes.use("/api/user", userRoutes);
+
 
 module.exports = handleRoutes;
