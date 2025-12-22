e67c6e69d7e2df09c47926becd019b9c20ed4082
hkrVikrant <vikranttomartech@gmail.com>
2025-12-22 11:38:18 +0530

    udpate middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,7 +7,10 @@ const protect = async (req, res, next) => {
     try {
         const token = req.headers.authorization?.split(" ")[1];
         if (!token)
-            return res.status(401).json({ message: "Not authorized" });
+            return res.status(401).json({
+                success: false,
+                message: "Not authorized"
+            });
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await User.findById(decoded.id).select("-password");
