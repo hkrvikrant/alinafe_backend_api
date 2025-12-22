d97c050d1fe2a4e9aa5b60f2b66bf95e895d216e
hkrVikrant <vikranttomartech@gmail.com>
2025-12-22 11:38:30 +0530

    Update user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,26 +1,40 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      require: true,
-    },
-    lastName: {
-      type: String,
-    },
-    email: {
-      type: String,
-      require: true,
-      unique: true,
-    },
-    gender: {
-      type: String,
-      require: true,
-    },
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true
   },
-  { timestamps: true }
-);
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["customer", "vendor", "admin"],
+    default: "customer"
+  },
+  phone: String,
+  avatar: String,
+
+  addresses: [
+    {
+      fullName: String,
+      addressLine: String,
+      city: String,
+      state: String,
+      country: String,
+      pinCode: String,
+      phone: String
+    }
+  ]
+}, { timestamps: true });
 
 const User = mongoose.model("user", userSchema);
 
