a6e276cef5cd192035c0c24fd5811fba5fcff129
hkrVikrant <vikranttomartech@gmail.com>
2025-12-22 11:38:39 +0530

    Added user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,18 +1,35 @@
 const express = require("express");
 const {
-  handleCreateNewUser,
-  handleGetAllUsers,
-  handleGetUserById,
-  handleUpdateUser,
-  handleDeleteUser,
+  getMyProfile,
+  updateMyProfile,
+  deleteMyAccount,
+  getAllUsers,
+  getUserById,
+  updateUserRole,
+  deleteUser,
 } = require("../controllers/user.controller");
+
+const {
+  protect,
+  isAdmin
+} = require("../middlewares/auth.middleware.js")
+
 
 const userRoutes = express.Router();
 
-userRoutes.get("/", handleGetAllUsers);
-userRoutes.get("/:id", handleGetUserById);
-userRoutes.post("/", handleCreateNewUser);
-userRoutes.patch("/", handleUpdateUser);
-userRoutes.delete("/", handleDeleteUser);
+
+userRoutes.get("/me", protect, getMyProfile);
+userRoutes.put("/me", protect, updateMyProfile);
+userRoutes.delete("/me", protect, deleteMyAccount);
+
+
+// Admin
+userRoutes.get("/", protect, isAdmin, getAllUsers);
+userRoutes.get("/:id", protect, isAdmin, getUserById);
+userRoutes.put("/role", protect, isAdmin, updateUserRole);
+userRoutes.delete("/:id", protect, isAdmin, deleteUser);
+
+
+
 
 module.exports = userRoutes;
