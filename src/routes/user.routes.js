afbe4ded040f5267a8164ba9b7eb3f70c0aa3b81
hkrVikrant <vikranttomartech@gmail.com>
2025-12-30 12:42:06 +0530

    Removwed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,16 +3,9 @@ const {
   getMyProfile,
   updateMyProfile,
   deleteMyAccount,
-  getAllUsers,
-  getUserById,
-  // updateUserRole,
-  deleteUser,
 } = require("../controllers/user.controller");
 
-const {
-  protect,
-  isAdmin
-} = require("../middlewares/auth.middleware.js")
+const { protect } = require("../middlewares/auth.middleware.js")
 
 
 const userRoutes = express.Router();
@@ -21,14 +14,6 @@ const userRoutes = express.Router();
 userRoutes.get("/", protect, getMyProfile);
 userRoutes.put("/", protect, updateMyProfile);
 userRoutes.delete("/", protect, deleteMyAccount);
-
-
-// Admin
-// userRoutes.get("/", protect, getAllUsers);
-// userRoutes.get("/:id", protect, getUserById);
-// // userRoutes.put("/role", protect, isAdmin, updateUserRole);
-// userRoutes.delete("/:id", protect, deleteUser);
-
 
 
 
