b6607f6ef8f738e28834576a6db936fa085f3eeb
hkrVikrant <vikranttomartech@gmail.com>
2025-12-30 12:51:19 +0530

    Added user API for admin

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,4 +1,5 @@
 const Staff = require("../models/staff.model");
+const User = require("../models/user.model");
 
 
 // GET ALL Staff
@@ -42,7 +43,101 @@ const updateStaffStatus = async (req, res) => {
 
 
 
+
+// GET ALL USERS
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find().select("-password");
+        res.status(200).json({
+            success: true,
+            data: users
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+// UDPATE USER
+const updateUserDetailsById = async (req, res) => {
+    try {
+        const {
+            id,
+            fullName,
+            phone,
+        } = req.body;
+
+        const user = await User.findByIdAndUpdate(
+            id,
+            { fullName, phone },
+            { new: true }
+        ).select("-password");
+
+        res.status(200).json({
+            success: true,
+            data: user,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+// GET USER BY ID
+const getUserById = async (req, res) => {
+    try {
+        const { id } = req.body;
+
+        const user = await User.findById(id).select("-password");
+        if (!user)
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+// DELETE USER
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.body;
+        await User.findByIdAndDelete(id);
+        res.status(200).json({
+            success: true,
+            message: "User deleted"
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+
+
+
 module.exports = {
     getAllStaff,
     updateStaffStatus,
+
+    getAllUsers,
+    updateUserDetailsById,
+    getUserById,
+    deleteUser,
+
 };
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,6 +3,10 @@ const express = require("express");
 const {
     getAllStaff,
     updateStaffStatus,
+    getAllUsers,
+    deleteUser,
+    getUserById,
+    updateUserDetailsById,
 } = require("../controllers/admin.controller");
 const {
     staffProtect,
@@ -15,6 +19,11 @@ const adminRoutes = express.Router();
 
 adminRoutes.get("/getAllStaff", staffProtect, isAdmin, getAllStaff);
 adminRoutes.post("/staffStatus", staffProtect, isAdmin, updateStaffStatus);
+
+adminRoutes.get("/getAllUser", staffProtect, isAdmin, getAllUsers);
+adminRoutes.post("/getUserById", staffProtect, isAdmin, getUserById);
+adminRoutes.post("/updateUserDetailsById", staffProtect, isAdmin, updateUserDetailsById);
+adminRoutes.delete("/deleteUser", staffProtect, isAdmin, deleteUser);
 
 
 
