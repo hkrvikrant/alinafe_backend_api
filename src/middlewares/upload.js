e133d3dc66f1f50bbd761b23d75de7863381dbaa
hkrVikrant <vikranttomartech@gmail.com>
2026-01-13 18:06:57 +0530

    update image upload middleware

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/categories");
+        cb(null, "uploads/images");
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + "-" + file.originalname);
