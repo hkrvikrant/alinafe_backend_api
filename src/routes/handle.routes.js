a28aa56d8fa4f78a08f60f0d903d5b9bb0595934
hkrVikrant <vikranttomartech@gmail.com>
2026-01-13 18:08:13 +0530

    update routes

--- src/routes/handle.routes.js
+++ src/routes/handle.routes.js
@@ -4,6 +4,7 @@ const authRoutes = require("./auth.routes");
 const userRoutes = require("./user.routes");
 
 const adminRoutes = require("./admin.routes");
+const vendorRoutes = require("./vendor.routes");
 
 const handleRoutes = express.Router();
 
@@ -11,10 +12,9 @@ const handleRoutes = express.Router();
 handleRoutes.use("/uploads", express.static("uploads"));
 
 handleRoutes.use("/api/auth", authRoutes);
-
 handleRoutes.use("/api/user", userRoutes);
-
 handleRoutes.use("/api/admin", adminRoutes);
+handleRoutes.use("/api/vendor", vendorRoutes);
 
 
 module.exports = handleRoutes;
