82a9e3a875984e715fe5671b03748426588508e6
hkrVikrant <vikranttomartech@gmail.com>
2026-01-13 18:07:26 +0530

    Added product model

---
+++ src/models/product.model.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    vendorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "staff",
+        required: true
+    },
+
+    categories: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "category",
+            required: true
+        }
+    ],
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    slug: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+
+    discountedPrice: {
+        type: Number
+    },
+
+    stock: {
+        type: Number,
+        required: true
+    },
+
+    sku: {
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    images: [String],
+
+    status: {
+        type: String,
+        enum: ["pending", "approved", "rejected"],
+        default: "pending"
+    },
+
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+
+}, { timestamps: true });
+
+const Product = mongoose.model("product", productSchema);
+
+module.exports = Product;
