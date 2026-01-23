ea4f9af1a81301e4db851051f74527ebdd3ecfdb
hkrVikrant <vikranttomartech@gmail.com>
2026-01-23 17:49:17 +0530

    Added cart routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -15,6 +15,14 @@ const {
   getProductsGroupedByParentCategory,
   getProductsByCategorySlug
 } = require("../controllers/products.controller.js");
+const {
+  getCartList,
+  addToCart,
+  updateCartItem,
+  removeCartItem,
+  clearCart,
+  mergeCart,
+} = require("../controllers/cart.controller.js");
 
 
 const userRoutes = express.Router();
@@ -33,6 +41,13 @@ userRoutes.post("/getProductsByCategoryId", getProductsByCategoryId);
 userRoutes.post("/getProductsByCategorySlug", getProductsByCategorySlug);
 // userRoutes.get("/getFeaturedProducts", getFeaturedProducts);
 
+userRoutes.post("/cart", getCartList);
+userRoutes.post("/cart/add", addToCart);
+userRoutes.post("/cart/delete", clearCart);
+userRoutes.put("/cart/update", updateCartItem);
+userRoutes.post("/cart/item", removeCartItem);
+
+userRoutes.post("/cart/merge", mergeCart);
 
 
 module.exports = userRoutes;
