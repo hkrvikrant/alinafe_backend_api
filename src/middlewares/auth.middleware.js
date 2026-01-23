5917f091644fbb09f0cb785b911bcaa0349a4140
hkrVikrant <vikranttomartech@gmail.com>
2026-01-23 19:05:25 +0530

    Added optionalAuth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -79,9 +79,27 @@ const isVendor = (req, res, next) => {
     }
 };
 
+const optionalAuth = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader?.startsWith("Bearer ")) {
+        try {
+            const token = authHeader.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const user = await User.findById(decoded.id).select("_id email");
+            req.user = user;
+        } catch (e) {
+            req.user = null;
+        }
+    }
+
+    next();
+};
+
 
 module.exports = {
     protect,
+    optionalAuth,
 
     staffProtect,
     isAdmin,
